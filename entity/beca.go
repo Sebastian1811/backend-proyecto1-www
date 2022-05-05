510879f1d2f0e542832fa63c1390969c338d4d07
Sebastian <eljhoan1411@gmail.com>
2022-05-05 16:25:15 -0500

    add beca entity

---
+++ entity/beca.go
@@ -0,0 +1,11 @@
+package entity
+
+// Añadir requerimientos, quizas como otra entidad porque son muchos.
+
+type Beca struct {
+	Categoria              string
+	Nombre                 string
+	PorcentajeFinanciacion string
+	Pais                   string
+	//Requisitos
+}

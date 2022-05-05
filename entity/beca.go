3420dce033283e8e9c077815aa953d708838591f
Sebastian <eljhoan1411@gmail.com>
2022-05-05 16:28:22 -0500

    add serializers beca

--- entity/beca.go
+++ entity/beca.go
@@ -3,9 +3,9 @@ package entity
 // Añadir requerimientos, quizas como otra entidad porque son muchos.
 
 type Beca struct {
-	Categoria              string
-	Nombre                 string
-	PorcentajeFinanciacion string
-	Pais                   string
+	Categoria              string `json:"categoria"`
+	Nombre                 string `json:"nombre"`
+	PorcentajeFinanciacion int    `json:"porcentajeF"`
+	Pais                   string `json:"pais"`
 	//Requisitos
 }

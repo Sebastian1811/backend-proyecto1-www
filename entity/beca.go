c9005e9e6d4fda6b8e6abc8d2727a8438004d4ab
Sebastian <eljhoan1411@gmail.com>
2022-05-26 16:09:28 -0500

    add field universidad

--- entity/beca.go
+++ entity/beca.go
@@ -9,6 +9,7 @@ type Beca struct {
 	Nombre                 string      `json:"nombre" binding:"required,gte=1,lte=30" gorm:"type:varchar(30)"`
 	PorcentajeFinanciacion int         `json:"porcentajeF" binding:"required,gte=1,lte=100" gorm:"type:int"`
 	Pais                   string      `json:"pais" binding:"required,gte=1,lte=40" gorm:"type:varchar(40)"`
+	Universidad            string      `json:"universidad" binding:"required,gte=1,lte=30" gorm:"type:varchar(30)"`
 	Requisitos             []Requisito `json:"requisitos,omitempty" gorm:"foreignKey:BecaID; references:ID"`
 	CreatedAt              time.Time   `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt              time.Time   `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
